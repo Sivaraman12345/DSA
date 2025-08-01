9aa1ec739ef024b7c8be02083943d45133cd79e7
Sivaraman12345 <sivaraman.m12345@gmail.com>
2025-08-01 14:53:51 +0530

    Practice reversing array using recursion

--- basicRecursion.cpp
+++ basicRecursion.cpp
@@ -1,4 +1,8 @@
 #include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
 using namespace std;
 
 void print(int i , int n){
@@ -38,14 +42,37 @@ int factorial(int n){
     if (n==0) return 1;
     return n*factorial(n-1);
 }
-
-
-
+vector<int> revarray(vector<int> arr){
+    reverse(arr.begin(),arr.end());
+    return arr;
+}
+void printvector(const vector<int>& arr){
+    for (int i:arr){
+        cout<<i<<' ';
+    }
+}
+void revarray1(int n,int i,vector<int>& arr){
+    if (i>=n/2) return;
+    int dup=arr[i];
+    arr[i]=arr[arr.size()-1-i];
+    arr[arr.size()-1-i]=dup;
+    revarray1(n,i+1,arr);
+}
 
 
 int main(){
     int n;
+    int k;
+    vector<int> arr;
     cin>>n;
-    cout<<(n);
+
+    for (int i=0;i<n;i++){
+        cin>>k;
+        arr.push_back(k);
+    }
+    
+    revarray1(n,0,arr);
+    printvector(arr);
+    
     return 0;
 }
